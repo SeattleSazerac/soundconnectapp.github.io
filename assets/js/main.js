493278519ae6ca9cd976d723e539e2b94a15302c
SeattleSazerac <45088242+SeattleSazerac@users.noreply.github.com>
2019-02-05 11:44:37 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/crowd-bg1.jpg': 'center',
+							'images/crowd-bg2.jpg': 'center',
+							'images/crowd-bg3.jpg': 'center'
 						},
 
 					// Delay.
